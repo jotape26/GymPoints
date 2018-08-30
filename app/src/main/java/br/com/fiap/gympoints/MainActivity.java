820c3630fad0eeb6ad4e3a6b61a54b1c777322bf
João Leite <joaopleite26@gmail.com>
2018-08-30 19:33:52 -0300

    Tela Registro (Unfinished)

--- app/src/main/java/br/com/fiap/gympoints/MainActivity.java
+++ app/src/main/java/br/com/fiap/gympoints/MainActivity.java
@@ -1,10 +1,15 @@
 package br.com.fiap.gympoints;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        txtRegistrar = (TextView)findViewById(R.id.cadastrar);
+
+        txtRegistrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrarActivity.class));
+            }
+        });
     }
 }
