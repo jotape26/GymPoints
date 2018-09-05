1d7335a50fe11200bebb89766183e865bab4a77c
WesFuzii <wesfuzii@gmail.com>
2018-09-04 23:06:01 -0300

    NavigationDrawer

--- app/src/main/java/br/com/fiap/gympoints/MainActivity.java
+++ app/src/main/java/br/com/fiap/gympoints/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtRegistrar;
+    private Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,19 @@ public class MainActivity extends AppCompatActivity {
         actionBar.hide();
 
         txtRegistrar = (TextView)findViewById(R.id.cadastrar);
+        btnEntrar = findViewById(R.id.btnEntrar);
 
         txtRegistrar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrarActivity.class));
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
             }
         });
     }
